f57930741b0896dee2f210520e84448c5912705c
Patrick Ehlert <patrick.ehlert@europeana.eu>
2020-03-23 16:35:46 +0100

    EA-1953 Remove id for sequence and image annotation in v2 manifests

--- src/main/java/eu/europeana/iiif/service/EdmManifestMapping.java
+++ src/main/java/eu/europeana/iiif/service/EdmManifestMapping.java
@@ -574,7 +574,7 @@ public final class EdmManifestMapping {
         }
         // there should be only 1 sequence, so sequence number is always 1
         eu.europeana.iiif.model.v2.Sequence[] result = new eu.europeana.iiif.model.v2.Sequence[1];
-        result[0] = new eu.europeana.iiif.model.v2.Sequence(Definitions.getSequenceId(europeanaId, 1));
+        result[0] = new eu.europeana.iiif.model.v2.Sequence();
         result[0].setStartCanvas(Definitions.getCanvasId(europeanaId, 1));
         result[0].setCanvases(canvases.toArray(new eu.europeana.iiif.model.v2.Canvas[0]));
         return result;
@@ -731,7 +731,7 @@ public final class EdmManifestMapping {
 
         // canvas has 1 annotation (image field)
         c.setImages(new eu.europeana.iiif.model.v2.Annotation[1]);
-        c.getImages()[0] = new eu.europeana.iiif.model.v2.Annotation(Definitions.getAnnotationId(europeanaId, order));
+        c.getImages()[0] = new eu.europeana.iiif.model.v2.Annotation();
         c.getImages()[0].setOn(c.getId());
 
         // annotation has 1 annotationBody
